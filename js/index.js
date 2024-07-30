e8b5cfeec43a98512bacd1c11b513782c902c957
Code Red <81372350+RedReddit07@users.noreply.github.com>
2024-07-30 16:28:55 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.scrollTo(0, 0);
+
+function copy() {
+  var copyText = "reynolderenofercol@gmail.com";
+  navigator.clipboard.writeText(copyText);
+  alert("Successfuly copied the email");
+}
